9f90bafa245b60d521ec2750dbbc1fc901f497de
Div685 <divyeshpatel685@gmail.com>
2024-01-22 02:26:51 +0300

    Add Header component
    - Style.css file

---
+++ app/workentitlement/visaVerficationQuery.aspx/page.jsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header';
+
+export const metadata = {
+  title: "Visa Verification Enquiry",
+};
+
+export default function Home2() {
+  return (
+    <>
+      <Header />
+      <h2>First Page</h2>
+    </>
+    
+  )
+}
