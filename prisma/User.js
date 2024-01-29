61f7b56cfd4b41b87f74b1a132262fd436fde088
Div685 <divyeshpatel685@gmail.com>
2024-01-30 02:29:09 +0300

    Add Create method for prisma dB

--- prisma/User.js
+++ prisma/User.js
@@ -2,16 +2,40 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-export const createUserData = async () => {
-    const user = await prisma.user.create()
+export const createUserData = async (loginName, familyName, dob, gender, visaType, visaStartDate, firstEntryBefore, passportNationality, passportNumber, clientNumber, visaExpiryDate, numberOfEntries, expirtyDate, pdfLink) => {
+  const user = await prisma.user.create({
+    loginName: loginName,
+    familyName: familyName,
+    firstName: firstName,
+    dob: dob,
+    gender: gender,
+    visaType: visaType,
+    visaStartDate: visaStartDate,
+    firstEntryBefore: firstEntryBefore,
+    passportNationality: passportNationality,
+    passportNumber: passportNumber,
+    clientNumber: clientNumber,
+    visaExpiryDate: visaExpiryDate,
+    numberOfEntries: numberOfEntries,
+    expirtyDate: expirtyDate,
+    pdfLink: pdfLink,
+  })
+
+  return user;
 }
 
-export const getUserData = async () => {
-    const users = await prisma.user.findUnique({
-        where: {
+export const getUserData = async (id) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id
+    }
+  })
+  return user;
+}
 
-        }
-    })
+export const getAllUser = async () => {
+  const users = await prisma.user.findMany()
+  return users;
 }
 
 /*main()
