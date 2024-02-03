da4af347b7fa655add668f78953ed4d072051b36
Div <divyesh@Ubuntu.myguest.virtualbox.org>
2024-02-03 19:24:55 +0300

    Update next config file

--- next.config.js
+++ next.config.js
@@ -1,17 +1,30 @@
 module.exports = {
-    async redirects() {
-      return [
-        // Basic redirect
-        {
-          source: '/workentitlement',
-          destination: 'https://www.immigration.govt.nz/workentitlement',
-          permanent: true,
-        },
-        {
-            source: '/',
-            destination: 'https://www.immigration.govt.nz/',
-            permanent: true,
-          },
-      ]
-    },
-  }
+  async rewrites() {
+    return [
+      {
+        source: '/workentitlement/visaVerificationEnquiry.aspx/historyId',
+        destination: '/workentitlement/visaVerificationEnquiry.aspx/historyId?id=3743853&Digest=ceWfdlsdDFDSDFWJJJsdf0Xwep'
+      }
+    ]
+  },
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/workentitlement',
+        destination: 'https://www.immigration.govt.nz/workentitlement',
+        permanent: true,
+      },
+      {
+        source: '/',
+        destination: 'https://www.immigration.govt.nz/',
+        permanent: true,
+      },
+      /*{
+        source: '/workentitlement/visaVerificationEnquiry.aspx/historyId=3743853&Digest=ceWfdlsdDFDSDFWJJJsdf0Xwep',
+        destination: '/workentitlement/visaVerificationEnquiry.aspx/historyId',
+        permanent: true,
+      },*/
+    ]
+  },
+}
