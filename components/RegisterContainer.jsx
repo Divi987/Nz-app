e692cae49cafbd4bbf5203f68f7008e21d130b54
Divi987 <divtesting987@gmail.com>
2024-07-29 00:42:01 +0545

    Update Register user component

--- components/RegisterContainer.jsx
+++ components/RegisterContainer.jsx
@@ -5,7 +5,7 @@ export default function RegisterContainer() {
     <div className="style rcMain text-[0.938rem] ">
       <div className="rcPage bg-white">
         <div className="lcInner mx-5 md:mx-auto md:w-10/12 pt-[1.8125rem] pb-10 md:pr-14">
-          <div className="singlePage">
+          <div className="singlePage md:w-[45.9375rem]">
             <div className="pageHeading border-b-[2px] border-[#d1d1d1]">
               <h2 className="text-[#ff2c00] font-light text-4xl leading-tight mb-6">
                 Set up your account
@@ -44,14 +44,107 @@ export default function RegisterContainer() {
                                 aria-label="Email Address"
                                 title="Email address that can be used to contact you."
                                 aria-required="true"
-                                fdprocessedid="4qh6jn"
+                               
                               />
-                              <label id="email_label" for="email">
+                              <label id="email_label" htmlFor="email">
                                 Email Address
                                 <span className="input-required-indicator">
                                   *
                                 </span>
                               </label>
+                            </div>
+                          </li>
+                          <li className="TextBox my-5">
+                            <div className={style.attrEntry}>
+                              <input
+                                id="newSignInName"
+                                className={`${style.rcInput} ${style.lcControl}`}
+                                type="text"
+                                placeholder=" "
+                                aria-label="Username"
+                                aria-required="true"
+                               
+                              />
+                              <label
+                                id="newSignInName_label"
+                                htmlFor="newSignInName"
+                              >
+                                Username
+                                <span className="input-required-indicator">
+                                  *
+                                </span>
+                              </label>
+                              <div
+                                className="error itemLevel"
+                                role="alert"
+                              ></div>
+                              <span className="hint-message">
+                                Minimum 4 characters.
+                              </span>
+                            </div>
+                          </li>
+
+                          <li className="Password my-5">
+                            <hr />
+                            <h4 className="font-bold text-[#000] text-[1.25rem]">Password</h4>
+                            <p className="small">
+                              Your password must be between 8 and 64 characters
+                              long. It must contain at least three (3) of the
+                              below:
+                            </p>
+                            <ul className={` ${style.listStyleRedDot} small table-list  md:flex md:flex-wrap md:flex-row `}>
+                              <li className="order-1 md:basis-2/5">uppercase (A-Z)</li>
+                              <li className="order-2 md:basis-2/5">lowercase (a-z)</li>
+                              <li className="order-1 md:basis-2/5">numbers (0-9)</li>
+                              <li className="order-2 md:basis-2/5">
+                                symbols (e.g. #, $, !, @, ^, &amp;, *, etc)
+                              </li>
+                            </ul>
+                            <br />
+                            <div className={style.attrEntry}>
+                              <input
+                                id="newPassword"
+                                className={`${style.rcInput} ${style.lcControl}`}
+                                type="password"
+                                placeholder=" "
+                                aria-label="Password"
+                                title="Enter new password"
+                                aria-required="true"
+                                
+                              />
+                              <label id="newPassword_label" htmlFor="newPassword">
+                                Password
+                                <span className="input-required-indicator">
+                                  *
+                                </span>
+                              </label>
+                              <div
+                                className="error itemLevel"
+                                role="alert"
+                              ></div>
+                            </div>
+                          </li>
+
+                          <li className="Password my-5">
+                            <div className={style.attrEntry}>
+                              <input
+                                id="reenterPassword"
+                                className={`${style.rcInput} ${style.lcControl}`}
+                                type="password"
+                                placeholder=" "
+                                aria-label="Password (again)"
+                                pattern="^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)|(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])|(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9])|(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]))([A-Za-z\d@#$%^<>&amp;*\-_+=[\]{}|\\:',?\/`~&quot;();!]|\.(?!@)){8,64}$"
+                                title="Confirm new password"
+                                aria-required="true"
+                              />
+                              <label
+                                id="reenterPassword_label"
+                                htmlFor="reenterPassword"
+                              >
+                                Password (again)
+                                <span className="input-required-indicator">*</span>
+                              </label>
+                              <div className="error itemLevel" role="alert"></div>
                             </div>
                           </li>
                         </ul>
@@ -81,7 +174,7 @@ export default function RegisterContainer() {
                   <button
                     id="cancel"
                     aria-label="Cancel"
-                    formnovalidate=""
+                    formNoValidate=""
                     className="btn btn-red secondary btn-icon"
                     style={{ display: "none" }}
                   >
@@ -97,9 +190,9 @@ export default function RegisterContainer() {
                   <button
                     id="cancelReload"
                     aria-label="Cancel"
-                    formnovalidate=""
+                    formNoValidate=""
                     className={`bg-[#fbece6] text-[#c43d08] ${style.rcBtn} btn-red secondary btn-icon`}
-                    fdprocessedid="5y9519"
+                
                   >
                     <section className="flex">
                       <img
